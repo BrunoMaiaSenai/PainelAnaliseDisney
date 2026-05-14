52f1c14a5cfd88b55e4f95389108a5849013f080
Bruno Maia <brunomaiadesenv@gmail.com>
2026-05-14 20:13:37 -0300

    Atualizando a Filme.cs

--- PainelAnaliseDisney/Models/Filme.cs
+++ PainelAnaliseDisney/Models/Filme.cs
@@ -8,5 +8,16 @@ namespace PainelAnaliseDisney.Models
 {
     internal class Filme
     {
+
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Films { get; set; }
+        public string? ShortFilms { get; set; }
+        public string? TvShows { get; set; }
+        public string? Url { get; set; }
+        public string? ImageUrl { get; set; }
+
+        // Data/hora em que o registro foi salvo
+        public string Timestamp { get; set; } = DateTime.Now.ToString("dd/MM/yyyy HH:mm");
     }
 }
