4a39902b0a75057ff952e4dffee019c51458b22a
Bruno Maia <brunomaiadesenv@gmail.com>
2026-05-14 20:06:17 -0300

    Atualizada BaseViewModel.cs

--- PainelAnaliseDisney/ViewModels/BaseViewModel.cs
+++ PainelAnaliseDisney/ViewModels/BaseViewModel.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PainelAnaliseDisney.ViewModels
 {
-    internal class BaseViewModel
+    public abstract class BaseViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
 }
