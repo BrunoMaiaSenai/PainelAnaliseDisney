15fee16ac486b3fbbbc216aebc36ec7abe26a065
Bruno Maia <brunomaiadesenv@gmail.com>
2026-05-18 15:14:56 -0300

    Atualizando a LogFilme

--- PainelAnaliseDisney/Services/LogFilme.cs
+++ PainelAnaliseDisney/Services/LogFilme.cs
@@ -1,12 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
+using PainelAnaliseDisney.Models;
+using PainelAnaliseDisney.Data;
 
 namespace PainelAnaliseDisney.Services
 {
     internal class LogFilme
     {
+        private readonly HttpClient _httpClient;
+
+        public LogFilme()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task SalvarNoFirebaseAsync(Filme filme)
+        {
+            try
+            {
+                // Envia uma requisição POST salvando o filme no histórico do Firebase Realtime DB
+                await _httpClient.PostAsJsonAsync(Database.FirebaseUrl, filme);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"Erro ao salvar Log no Firebase: {ex.Message}");
+            }
+        }
     }
 }
