b3efcbe360062e448a05786ba82f05b032a6ae98
Bruno Maia <brunomaiadesenv@gmail.com>
2026-05-18 15:17:42 -0300

    Atualizando a MainViewModel.cs

--- PainelAnaliseDisney/ViewModels/MainViewModel.cs
+++ PainelAnaliseDisney/ViewModels/MainViewModel.cs
@@ -1,12 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.ObjectModel;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using PainelAnaliseDisney.Commands;
+using PainelAnaliseDisney.Models;
+using PainelAnaliseDisney.Respositories;
 
 namespace PainelAnaliseDisney.ViewModels
 {
-    internal class MainViewModel
+    internal class MainViewModel : BaseViewModel
     {
+        private readonly FilmeRepository _repository;
+        private ObservableCollection<Filme> _dados;
+        private bool _isCarregando;
+
+        public ObservableCollection<Filme> Dados
+        {
+            get => _dados;
+            set
+            {
+                _dados = value;
+                OnPropertyChanged(nameof(Dados));
+            }
+        }
+
+        public bool IsCarregando
+        {
+            get => _isCarregando;
+            set
+            {
+                _isCarregando = value;
+                OnPropertyChanged(nameof(IsCarregando));
+            }
+        }
+
+        public ICommand BuscarCommand { get; }
+        public ICommand LimparCommand { get; }
+
+        public MainViewModel()
+        {
+            _repository = new FilmeRepository();
+            _dados = new ObservableCollection<Filme>();
+
+            // Vinculando comandos com a classe RelayCommand fornecida
+            BuscarCommand = new RelayCommand(async () => await ExecutarBuscaAsync());
+            LimparCommand = new RelayCommand(() => ExecutarLimpeza());
+        }
+
+        private async Task ExecutarBuscaAsync()
+        {
+            if (IsCarregando) return;
+
+            IsCarregando = true;
+            Dados.Clear();
+
+            var resultado = await _repository.ObterEGravarFilmesAsync();
+
+            foreach (var filme in resultado)
+            {
+                Dados.Add(filme);
+            }
+
+            IsCarregando = false;
+        }
+
+        private void ExecutarLimpeza()
+        {
+            Dados.Clear();
+        }
     }
 }
